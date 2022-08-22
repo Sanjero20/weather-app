55693ad2201293213aaa80aac00d9c01772a5b29
Sanjero <gelosantos72@gmail.com>
2022-08-22 23:05:53 +0800

    Allow user to search for specific location

--- src/index.js
+++ src/index.js
@@ -1,21 +1,55 @@
 import { getWeatherData } from './weather';
 
-const location = 'Manila';
-
-// getWeatherData(location).then(data => {
-//   console.log(data);
-// });
-
+const searchInput = document.querySelector('input[name="location"]');
 const searchBar = document.querySelector('.search-bar');
 const mainContainer = document.querySelector('.weather-info');
 const loadingBar = document.querySelector('.loader');
 
-searchBar.classList.add('adjust');
+const location = mainContainer.querySelector('.location');
+const temp = mainContainer.querySelector('.temp');
+
+const type = mainContainer.querySelector('.type');
+const icon = mainContainer.querySelector('.icon');
+const desc = mainContainer.querySelector('.description');
+
+function fetchData(location) {
+  getWeatherData(location).then(data => {
+    loadingBar.classList.add('hide');
+    setUpWeatherComponent(data);
+  });
+}
 
 searchBar.addEventListener('submit', e => {
   e.preventDefault();
-  searchBar.classList.add('adjust');
 
+  if (searchInput.value === '') return;
+
+  searchBar.classList.add('adjust');
   mainContainer.classList.remove('hide');
   loadingBar.classList.remove('hide');
+
+  clearContainer();
+  fetchData(searchInput.value);
 });
+
+function setUpWeatherComponent(data) {
+  const component = document.querySelector('.weather-data');
+  component.classList.remove('hide');
+
+  location.textContent = `${data.name}, ${data.sys.country}`;
+  temp.innerHTML = `${(data.main.temp - 273).toFixed(1)}&deg;C`;
+
+  type.textContent = data.weather[0].main;
+  desc.textContent = data.weather[0].description;
+
+  const iconURL = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  icon.src = iconURL;
+}
+
+function clearContainer() {
+  location.textContent = '';
+  temp.innerHTML = '';
+  type.textContent = '';
+  desc.textContent = '';
+  icon.src = '';
+}
